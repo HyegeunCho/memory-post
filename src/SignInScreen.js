96f960b3b9ebc78d3515bbde3c97bcf35a22079b
HyegeunCho <hgcho84.kr@gmail.com>
2018-06-19 09:38:58 +0900

    StyledFirebaseAuth 기능 추가 및 테스트 출력

---
+++ src/SignInScreen.js
@@ -0,0 +1,56 @@
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import React, {Component} from 'react';
+
+export default class SignInScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isSignedIn: false
+        };
+
+        this.uiConfig = {
+            // Popup signin flow rather than redirect flow
+            signInFlow: 'popup',
+            // Redirect to /signedIn after sign in is successful.
+            // Alternatively you can provide a callbacks.signInSuccess function
+            singinInSuccessUrl: '/signedIn', 
+            // We will display Google and Facebook as auth proviers.
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID, 
+                firebase.auth.FacebookAuthProvider.PROVIDER_ID
+            ]
+        };
+    }
+
+    // Listen to the FIrebase Auth state and set the local state.
+    componentDidMount() {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged((user) => this.setState({isSignedIn: !!user}));
+    }
+
+    // Make sure we un-regiseter Firebase boservers when the component unmounts.
+    componentWillUnmount() {
+        this.unregisterAuthObserver();
+    }
+
+    render() {
+        if (!this.state.isSignedIn) {
+            return (
+                <div>
+                    <h1>Memory Post</h1>
+                    <p>Please sign-in</p>
+                    <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <h1>Memory Post</h1>
+                <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+                <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SignInScreen from './SignInScreen';
+
+var config = {
+    apiKey: "AIzaSyC3JCUu3ixuiKFef9isK2GlXYnhY6QCQ14",
+    authDomain: "memorypost-hgcho.firebaseapp.com",
+    databaseURL: "https://memorypost-hgcho.firebaseio.com",
+    projectId: "memorypost-hgcho",
+    storageBucket: "memorypost-hgcho.appspot.com",
+    messagingSenderId: "862885935879"
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI
+ReactDOM.render(<SignInScreen/>, document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
